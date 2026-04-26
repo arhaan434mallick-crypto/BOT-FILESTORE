780b9791dd7b0edce3029bc935d026849bf577b3
arhaan434mallick-crypto <Arhaan434mallick@gmail.com>
2026-04-26 21:12:28 +0530

    Updated config with DB and API vars

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ LOGGER = True
 PORT = int(os.environ.get("PORT", 8080))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7500587425"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+# Some bots use these names instead:
+DB_URI = os.environ.get("DATABASE_URL", "")
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+
+# Shortener Vars
 SHORT_URL = os.environ.get("SHORT_URL", "")
 SHORT_API = os.environ.get("SHORT_API", "")
 SHORT_TUT = os.environ.get("SHORT_TUT", "")

d14c78e2ee31f94e8e7725cd7edf2148292a1e2f
arhaan434mallick-crypto <Arhaan434mallick@gmail.com>
2026-04-26 21:04:30 +0530

    Fixed LOGGER import in config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
-class Config(object):
-    LOGGER = True
-    PORT = int(os.environ.get("PORT", 8080))
-    OWNER_ID = int(os.environ.get("OWNER_ID", "7500587425"))
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SHORT_URL = os.environ.get("SHORT_URL", "")
-    SHORT_API = os.environ.get("SHORT_API", "")
-    SHORT_TUT = os.environ.get("SHORT_TUT", "")
+LOGGER = True
+PORT = int(os.environ.get("PORT", 8080))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7500587425"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+SHORT_URL = os.environ.get("SHORT_URL", "")
+SHORT_API = os.environ.get("SHORT_API", "")
+SHORT_TUT = os.environ.get("SHORT_TUT", "")

fb7cb0005f72b7454551ae1e6407138508ca1d4c
arhaan434mallick-crypto <Arhaan434mallick@gmail.com>
2026-04-26 21:21:32 +0530

    Added SESSION variable

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ PORT = int(os.environ.get("PORT", 8080))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7500587425"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
-# Some bots use these names instead:
 DB_URI = os.environ.get("DATABASE_URL", "")
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", 2040))
+API_HASH = os.environ.get("API_HASH", "b18441a1ff607e106cf4ead0914d7a42")
+
+# The missing variable
+SESSION = os.environ.get("SESSION", "Alya_Bot")
 
 # Shortener Vars
 SHORT_URL = os.environ.get("SHORT_URL", "")
